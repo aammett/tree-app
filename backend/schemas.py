b2f9b8f538a298cbe41d68818f1e1165a04a6b11
amineet001 <amine.ettafs@student-cs.fr>
2024-11-07 12:23:33 +0100

    remove unecessary code

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,33 +1,4 @@
-from pydantic import BaseModel, Field
-from typing import Optional, Dict, Any
-from datetime import datetime
-
-
-class TreeBase(BaseModel):
-    management_domain: Optional[str] = None
-    arrondissement: Optional[str] = None
-    address_complement: Optional[str] = None
-    site_address: Optional[str] = None
-    circumference_cm: Optional[int] = None
-    height_m: Optional[int] = None
-    species_variety: Optional[str] = None
-    cultivar: Optional[str] = None
-    type: Optional[str] = None
-    planted_on: Optional[datetime] = None
-    french_name: Optional[str] = None
-    geo_shape: Optional[Dict[str, Any]] = None
-
-
-class ArbreCreate(TreeBase):
-    pass
-
-
-class Arbre(TreeBase):
-    id: int = Field(..., description="Identifiant unique de l'arbre")
-
-    class Config:
-        from_attributes = True
-
+from pydantic import BaseModel
 
 class ArrondissementTreeCount(BaseModel):
     arrondissement: str
